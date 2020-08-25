dc3fbeb7a94ba59ca59d3a349c34ccfa5c3dcee7
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-08-25 10:04:32 -0400

    header, footer y primera pantalla

--- hootie-hoo/src/App.js
+++ hootie-hoo/src/App.js
@@ -4,14 +4,25 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+// import Background from './Components/Background';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Buttons from './Components/Buttons';
+import Welcome from './Components/Welcome';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <div className="App">
-          <h1>Hootie-Hoo</h1>
-        </div>
+
+        <Route path="/" exact>
+          <Header />
+          <Buttons />
+          <Welcome />
+          <Footer/>
+          {/* <Background /> */}
+        </Route>
+
       </Switch>
     </Router>
   );
---
+++ hootie-hoo/src/Components/Background.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Styles/Background.css';
+
+const Background = () => {
+    return (
+        <div id="background"></div>
+    )
+}
+
+export default Background
