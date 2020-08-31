c4047ce87e925af4d1e92fad18d9850d9ebc2244
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-08-31 17:57:24 -0400

    descargables juego funcionales

--- hootie-hoo/src/Components/Game/EndGame.jsx
+++ hootie-hoo/src/Components/Game/EndGame.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import { messages } from "./messages";
-import download1 from '../../Downloadables/Dibujo1.png'
-import download2 from '../../Downloadables/Dibujo2.png'
-import download3 from '../../Downloadables/Dibujo3.png'
-import download4 from '../../Downloadables/Dibujo4.png'
-
 
 class EndGame extends React.Component {
   handleClick = () => {
@@ -17,11 +12,10 @@ class EndGame extends React.Component {
         <div className="message">
           <h2>{messages[0].title}</h2>
           <p>{messages[0].message}</p>
-          <a  href={download1, download2, download3, download4} download={messages[0].download}>
-          <button>
-            descargar
-                              
-          </button>
+          <a href={messages[0].href}  download={messages[0].download}>
+            <button>
+              descargar
+            </button>
           </a> 
           <button className="btn btn-primary" onClick={this.handleClick}>
             Go Again
