b405de76b36888a9edfd0969191fc351e7fc60fa
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-09-01 12:48:14 -0400

    imágenes medallas

--- hootie-hoo/src/Components/Game/MemoryGame.jsx
+++ hootie-hoo/src/Components/Game/MemoryGame.jsx
@@ -36,8 +36,12 @@ class MemoryGame extends React.Component {
                 </div>
                 <div className="detractor-container"> <p className="detractor-title">Medallas</p> 
                 <p className="detractor-score">
-                {score === 1 ? <img src="https://i.ibb.co/bHZ3mHY/dddd.png" alt="Medal" className="medal-score"/> : null}
-                {score === 2 ? <img src="https://i.ibb.co/fYTh8Hm/Group-11.png" alt="Medal" className="medal2-score"/> : null}
+                  {score === 1 ? <img src="https://i.ibb.co/bHZ3mHY/dddd.png" alt="Medal" className="medal-score"/> : null}
+                  {score === 2 ? <img src="https://i.ibb.co/kX5jM64/Group-11.png" alt="Medal" className="medal2-score"/> : null}
+                  {score === 3 ? <img src="https://i.ibb.co/d6f2y1G/Group-111.png" alt="Medal" className="medal3-score"/> : null}
+                  {score === 4 ? <img src="https://i.ibb.co/5K0DCMV/Group-1111.png" alt="Medal" className="medal4-score"/> : null}
+                  {score === 5 ? <img src="https://i.ibb.co/P4bJPZH/Group-11111.png" alt="Medal" className="medal5-score"/> : null}
+                  {score >= 6 ? <img src="https://i.ibb.co/sFnLZ7S/Group-111111.png" alt="Medal" className="medal6-score"/> : null}
                 </p>
                 </div>
               </div>
